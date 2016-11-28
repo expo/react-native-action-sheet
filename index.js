5fae3565fc9d601bba37d618f62841fee70a0e46
jschloer <jschloer@terragotech.com>
2016-11-28 12:38:20 -0500

    const exports looks like it was a bad idea.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const exports = require('./ActionSheet');
-exports.ActionSheetProvider = require('./ActionSheetProvider').default;
-exports.connectActionSheet = require('./connectActionSheet').default;
+module.exports = require('./ActionSheet');
 
-
-module.exports = exports;
+module.exports.ActionSheetProvider = require('./ActionSheetProvider').default;
+module.exports.connectActionSheet = require('./connectActionSheet').default;
