7fe0aff0fe83ce093bb25884ee5c0b4d61fce032
Justin Stanley <justin.stanley@gmail.com>
2018-08-20 19:38:06 -0500

    Use shorthand for null to set title and message to undefined.
    Also undid change to formatting of return in render.

--- ActionSheet.ios.js
+++ ActionSheet.ios.js
@@ -5,18 +5,15 @@ import { ActionSheetIOS, View } from 'react-native';
 
 export default class ActionSheet extends React.Component {
   render() {
-    return (
-      <View style={{ flex: 1 }}>
-        {React.Children.only(this.props.children)}
-        </View>
-    );
+    return <View style={{ flex: 1 }}>{React.Children.only(this.props.children)}</View>;
   }
 
   showActionSheetWithOptions(options, onSelect) {
     const iosOptions = {
       ...options,
-      title: options.title === null ? undefined : options.title,
-      message: options.message === null ? undefined : options.message,
+      // A null title or message on iOS causes a crash
+      title: options.title || undefined,
+      message: options.message || undefined,
     };
 
     ActionSheetIOS.showActionSheetWithOptions(iosOptions, onSelect);
