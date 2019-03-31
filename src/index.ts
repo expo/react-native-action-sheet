41a06917b0ef2cb2269bb9e11ce7d8df2871f4c6
nd-02110114 <nd.12021218@gmail.com>
2019-03-31 11:32:32 +0900

    :recycle: (src) remove unused export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { default } from './ActionSheet';
 export { default as ActionSheetProvider } from './ActionSheetProvider';
 export { default as connectActionSheet } from './connectActionSheet';
 export * from './types';
