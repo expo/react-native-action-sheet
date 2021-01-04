f6a3925f4d5afae9fff0470ff00377331819f761
amauryliet <amaury.liet@hotmail.fr>
2021-01-04 18:12:15 +0100

    chore: Make build available to github-downloaders

---
+++ lib/typescript/types.d.ts
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { TextStyle, ViewStyle } from 'react-native';
+export interface ActionSheetProps {
+    showActionSheetWithOptions: (options: ActionSheetOptions, callback: (i: number) => void | Promise<void>) => void;
+}
+export interface ActionSheetIOSOptions {
+    options: string[];
+    title?: string;
+    message?: string;
+    tintColor?: string;
+    cancelButtonIndex?: number;
+    destructiveButtonIndex?: number;
+    anchor?: number;
+}
+export interface ActionSheetOptions extends ActionSheetIOSOptions {
+    icons?: React.ReactNode[];
+    tintIcons?: boolean;
+    textStyle?: TextStyle;
+    titleTextStyle?: TextStyle;
+    renderTitle?: () => React.ReactNode;
+    messageTextStyle?: TextStyle;
+    autoFocus?: boolean;
+    showSeparators?: boolean;
+    containerStyle?: ViewStyle;
+    separatorStyle?: ViewStyle;
+    useModal?: boolean;
+    destructiveColor?: string;
+}
