78ed30293ac3c3b545f196ea231f24e209fcaecb
Simek <gosimek@gmail.com>
2021-09-14 23:19:39 +0200

    fix ESLint autofix mischief

--- src/ActionSheet/index.tsx
+++ src/ActionSheet/index.tsx
@@ -214,6 +214,7 @@ export default class ActionSheet extends React.Component<Props, State> {
     }
 
     if (typeof options.cancelButtonIndex === 'undefined') {
+      return false;
     } else if (typeof options.cancelButtonIndex === 'number') {
       return this._onSelect(options.cancelButtonIndex);
     } else {
