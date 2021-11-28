7c2e90a8833d96f8c0b661add7fc5305610837a5
Jonathan Baudanza <jon@jonb.org>
2021-11-28 10:38:50 -0800

    Stabilize the idenitity of the Provider value

--- src/ActionSheetProvider.tsx
+++ src/ActionSheetProvider.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import ActionSheet from './ActionSheet';
 import { Provider } from './context';
-import { ActionSheetOptions } from './types';
+import { ActionSheetOptions, ActionSheetProps } from './types';
 
 interface Props {
   children: React.ReactNode;
@@ -11,24 +11,27 @@ interface Props {
 
 export default class ActionSheetProvider extends React.Component<Props> {
   _actionSheetRef: React.RefObject<ActionSheet>;
+  _context: ActionSheetProps;
 
   constructor(props: Props) {
     super(props);
     this._actionSheetRef = React.createRef();
-  }
-
-  getContext = () => {
-    return {
+    this._context = {
       showActionSheetWithOptions: (options: ActionSheetOptions, callback: (i: number) => void) => {
-        this._actionSheetRef.current !== null &&
+        if (this._actionSheetRef.current != null) {
           this._actionSheetRef.current.showActionSheetWithOptions(options, callback);
+        }
       },
     };
-  };
+  }
+
+  getContext(): ActionSheetProps {
+    return this._context;
+  }
 
   render() {
     return (
-      <Provider value={this.getContext()}>
+      <Provider value={this._context}>
         <ActionSheet ref={this._actionSheetRef} useNativeDriver={this.props.useNativeDriver}>
           {React.Children.only(this.props.children)}
         </ActionSheet>
